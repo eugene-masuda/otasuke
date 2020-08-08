c545847f6e00aa633a4fae2ec4403f5d5995ac91
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-08 11:31:48 +0900

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'high_scores#index'
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'high_scores/index'
 
   resources :high_scores
-  get '/pages/home'
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
